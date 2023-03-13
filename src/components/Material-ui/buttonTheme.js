93135742bb380da649364e488e36be8c9bc5a967
gowtham0927 <gowthamsaiyadlapalli1234@gmail.com>
2023-03-13 21:03:48 +0530

    button

---
+++ src/components/Material-ui/buttonTheme.js
@@ -0,0 +1,10 @@
+import { createTheme } from '@mui/material/styles';
+import { green, red } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: red,
+    secondary: green,
+  },
+});
+export default theme
